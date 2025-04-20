a8542e21ccf8df82183610d77d5f0ec8c4ede82c
Konstantin Otinov <kotinov@dar.io>
2025-04-20 09:47:47 +0300

    added websocket route

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const router = require('./router/index');
 const errorMiddleware = require('./middlewares/error-middleware');
-
 const PORT = process.env.PORT || 5000;
 const app = express();
+const MessageModel = require('./models/message-model');
+const WSServer = require('express-ws')(app);
+const aWss = WSServer.getWss();
 
 app.use(express.json());
 app.use(
@@ -19,6 +21,28 @@ app.use(
 app.use(cookieParser());
 app.use('/api', router);
 app.use(errorMiddleware);
+
+app.ws('/', (ws) => {
+  ws.on('message', (message) => {
+    const parsedMessage = JSON.parse(message);
+    switch (parsedMessage.event) {
+      case 'connect':
+      case 'disconnect':
+        broadcastMessage(parsedMessage);
+        break;
+      case 'message':
+        broadcastMessage(parsedMessage);
+        MessageModel.create(parsedMessage);
+        break;
+    }
+  });
+});
+
+const broadcastMessage = (message) => {
+  aWss.clients.forEach((client) => {
+    client.send(JSON.stringify(message));
+  });
+};
 
 const start = async () => {
   try {
